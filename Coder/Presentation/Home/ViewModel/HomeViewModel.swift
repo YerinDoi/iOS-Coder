998800975b2e7f120a984f5f55d77a4d5c9bbecc
yerin <dpfls1022@naver.com>
2024-08-19 18:46:44 +0900

    MVVM 구조 생성

---
+++ Coder/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Coder
+//
+//  Created by 박예린 on 8/19/24.
+//
+
+import Foundation
---
+++ Coder/Presentation/MyPage/ViewModel/MyPageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageViewModel.swift
+//  Coder
+//
+//  Created by 박예린 on 8/19/24.
+//
+
+import Foundation
